e2c16ddfa65159e0a50205271e38317c7dc5bb97
Vini <ixxvinicius@gmail.com>
2023-03-29 09:32:22 -0300

    Variaveis em Ts

--- build/index.js
+++ build/index.js
@@ -1,1 +1,6 @@
 "use strict";
+let ligado = true;
+let nome = "Vinicius";
+let idade = 19;
+let altura = 1.85;
+console.log(`Meu nome é ${nome} tenho ${idade} anos e ${altura} de altura`);
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+//Tipos Primitivos
+let ligado: boolean = true;
+let nome: string = "Vinicius";
+let idade: number = 19;
+let altura: number = 1.85;
+
+console.log(`Meu nome é ${nome} tenho ${idade} anos e ${altura} de altura`)
+
+//Tipos Especiais
+let nulo: null = null
+let indefinido: undefined = undefined
+
+//Tipos Abrangentes
+let ani: any = "Qualquer coisa"
+let semRetorno: void 
